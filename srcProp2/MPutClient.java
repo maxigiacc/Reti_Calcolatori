db8d37e83e0e40036116de2a3ce3881cacf2e6ab
maxigiacc <132470332+maxigiacc@users.noreply.github.com>
2024-02-09 13:17:35 +0100

    Add client
    Aggiunto client

--- srcProp2/MPutClient.java
+++ srcProp2/MPutClient.java
@@ -3,6 +3,7 @@ package Es2.srcProp2;
 // PutFileClient.java
 
 import java.net.*;
+
 import java.io.*;
 
 public class MPutClient {
@@ -11,13 +12,15 @@ public class MPutClient {
    
 		InetAddress addr = null;
 		int port = -1;
+        int sizeMax = 0;
 		
 		try{ //check args
-			if(args.length == 2){
+			if(args.length == 3){
 				addr = InetAddress.getByName(args[0]);
 				port = Integer.parseInt(args[1]);
+                sizeMax = Integer.parseInt(args[2]);
 			} else{
-				System.out.println("Usage: java PutFileClient serverAddr serverPort");
+				System.out.println("Usage: java PutFileClient serverAddr serverPort sizeMax");
 				System.exit(1);
 			}
 		} //try
@@ -25,7 +28,7 @@ public class MPutClient {
 		catch(Exception e){
 			System.out.println("Problemi, i seguenti: ");
 			e.printStackTrace();
-			System.out.println("Usage: java PutFileClient serverAddr serverPort");
+			System.out.println("Usage: java PutFileClient serverAddr serverPort sizeMax");
 			System.exit(2);
 		}
 		
@@ -36,16 +39,21 @@ public class MPutClient {
 		DataInputStream inSock = null;
 		DataOutputStream outSock = null;
 		String nomeFile = null;
+        long size = 0;
+        File fileCurr = null;
 
 		// creazione stream di input da tastiera
 		BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
 		System.out
-		    .print("PutFileClient Started.\n\n^D(Unix)/^Z(Win)+invio per uscire, oppure immetti nome file: ");
+		    .print("MPutClient Started.\n\n^D(Unix)/^Z(Win)+invio per uscire, oppure immetti nome file: ");
 
 		try{
 			while ( (nomeFile=stdIn.readLine()) != null){
-				// se il file esiste creo la socket
-				if(new File(nomeFile).exists()){
+                //se la dimensione massima è maggiore di zero creo la socket
+                // se il file esiste creo la socket
+                fileCurr = new File(nomeFile);
+                size = fileCurr.length();
+				if(fileCurr.exists()){
 					// creazione socket
 					try{
 						socket = new Socket(addr, port);
@@ -84,7 +92,103 @@ public class MPutClient {
 					// il client continua l'esecuzione riprendendo dall'inizio del ciclo
 					continue;
 				}
+
+                //invio dimensione massima
+                try {
+                    outSock.writeUTF("" + sizeMax);
+                }
+                catch(Exception e) {
+                    System.out
+		        		.println("Problemi nel passaggio del nome della dimensione massima");
+		        	e.printStackTrace();
+		        	// il server continua l'esecuzione riprendendo dall'inizio del ciclo
+		        	continue;
+                }
+
+                //ricezione esito dimensione massima
+                String esitoDimMax = null;
+                try {
+                    esitoDimMax = inSock.readUTF();
+                    if(!esitoDimMax.equalsIgnoreCase("ok")) {
+                        System.out.println("Problemi nella comunicazione dell'esito dim max");
+                        continue;
+                    }
+                    //invio nome File
+                    else {
+                        try {
+                            outSock.writeUTF(nomeFile);
+                        }
+                        catch(Exception e) {
+                            System.out.println("Problemi nella comunicazione del nome del file " + nomeFile);
+                            e.printStackTrace();
+                            continue;
+                        }
+                        
+                    } 
+                }
+                catch(Exception e) {
+                    System.out.println("Problemi comunicazione esito dimensione massima");
+                    continue;
+                }
+
+                //ricezione esito nome file
+                String esitoNome = null;
+                try {
+                    esitoNome = inSock.readUTF();
+                    if(esitoNome.equalsIgnoreCase("salta")) {
+                        System.out.println("File saltato");
+                        continue;
+                    }
+                    //invio nome File
+                    else {
+                        if(esitoNome.equalsIgnoreCase("attiva")) {
+                            //invio dimensione del file
+                            try {
+                                outSock.writeUTF(""+ size);
+                            }
+                            catch(Exception e) {
+                                System.out.println("Problemi nella comunicazione della dimensione del file " + nomeFile);
+                                e.printStackTrace();
+                                continue;
+                            }
+                        }
+                    } 
+                }
+                catch(Exception e) {
+                    System.out.println("Problemi comunicazione esito nome file");
+                    continue;
+                }
 				
+                //ricezione esito dimensione del file
+                String esitoDim = null;
+                try {
+                    esitoDim = inSock.readUTF();
+                    if(!esitoDim.equalsIgnoreCase("ok")) {
+                        System.out.println("File troppo piccolo");
+                        continue;
+                    }
+                    //invio nome File
+                    else {
+                        if(esitoNome.equalsIgnoreCase("attiva")) {
+                            //invio dimensione del file
+                            try {
+                                outSock.writeUTF(""+ size);
+                            }
+                            catch(Exception e) {
+                                System.out.println("Problemi nella comunicazione della dimensione del file " + nomeFile);
+                                e.printStackTrace();
+                                continue;
+                            }
+                        }
+                    } 
+                }
+                catch(Exception e) {
+                    System.out.println("Problemi comunicazione esito nome file");
+                    continue;
+                }
+
+                System.out.println("Inizio la trasmissione di " + nomeFile);
+
 				/* Invio file richiesto e attesa esito dal server */
 				// creazione stream di input da file
 				try{
@@ -105,27 +209,26 @@ public class MPutClient {
 					// il client continua l'esecuzione riprendendo dall'inizio del ciclo
 					continue;
 				}
-
-				// trasmissione del nome
-				try{
-					outSock.writeUTF(nomeFile);
-					System.out.println("Inviato il nome del file " + nomeFile);
-				}
-				catch(Exception e){
-					System.out.println("Problemi nell'invio del nome di " + nomeFile
-						+ ": ");
-					e.printStackTrace();
-					System.out
-				      	.print("\n^D(Unix)/^Z(Win)+invio per uscire, oppure immetti nome file: ");
-					// il client continua l'esecuzione riprendendo dall'inizio del ciclo
-					continue;
-				}
 				
-				System.out.println("Inizio la trasmissione di " + nomeFile);
 				
 				// trasferimento file
 				try{
-					inFile.close(); 			// chiusura file
+					// ciclo di lettura da sorgente e scrittura su destinazione
+                    int buffer = 0;   
+                    DataInputStream src = new DataInputStream(inFile); 
+                    try {
+                        // esco dal ciclo all lettura di un valore negativo -> EOF
+                        // N.B.: la funzione consuma l'EOF
+                        while ((buffer=src.read()) >= 0) {
+                            outSock.write(buffer);
+                        }
+                        outSock.flush();
+                    }
+                    catch (EOFException e) {
+                        System.out.println("Problemi, i seguenti: ");
+                        e.printStackTrace();
+                    }
+                    inFile.close(); 			// chiusura file
 					socket.shutdownOutput(); 	// chiusura socket in upstream, invio l'EOF al server
 					System.out.println("Trasmissione di " + nomeFile + " terminata ");
 				}
